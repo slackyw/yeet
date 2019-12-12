b45cea7fc64e6461a8a6bc4a5d7d6698c59f6316
slackyw <58596017+slackyw@users.noreply.github.com>
2019-12-12 10:43:30 +0100

    Add files via upload

--- yeet.sh
+++ yeet.sh
@@ -2,14 +2,17 @@
 
 # sudo apt install espeak beep -y;git clone "https://github.com/slackyw/yeet";cd yeet/;bash yeet.sh 60 "pikatchu, attak eclair" 3 20 300 2000
 
-export DISPLAY=:0
-amixer sset 'Master' unmute 1>/dev/null
-amixer sset 'Master' $1% 1>/dev/null
-espeak "$2"
-outp=`xrandr | grep " con" | awk '{print $1}'`
-for i in `seq 1 $3`
-do
-	xrandr --output $outp --gamma 0.$((RANDOM%9+1)):0.$((RANDOM%9+1)):0.$((RANDOM%9+1))
-	sudo env -i beep -l $4 -f $((RANDOM%($6-$5)+$5))
-done
+if [ $# -eq 6 ]; then
+	export DISPLAY=:0
+	amixer sset 'Master' unmute 1>/dev/null
+	amixer sset 'Master' $1% 1>/dev/null
+	espeak "$2"
+	outp=`xrandr | grep " con" | awk '{print $1}'`
+	for i in `seq 1 $3`
+	do
+		xrandr --output $outp --gamma 0.$((RANDOM%9+1)):0.$((RANDOM%9+1)):0.$((RANDOM%9+1))
+		sudo env -i beep -l $4 -f $((RANDOM%($6-$5)+$5))
+	done
 xrandr --output $outp --gamma 1:1:1
+else echo "Usage of yeet : bash yeet.sh [volume] [phrase] [nb beep] [longueur beep] [freq min] [freq max]"
+fi
