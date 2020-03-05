d7c69f11be5662a2dc7b0ac352181c11c1f4b27e
floriangarciasoto <floriangarciasoto@outlook.fr>
2020-03-05 15:52:26 +0100

    onestla

--- piano.sh
+++ piano.sh
@@ -2,36 +2,14 @@
 
 echo "LE PIANO DU YEET !!!"
 
+declare -A idx=(['q']=0 ['s']=1 ['d']=2 ['f']=3 ['g']=4 ['h']=5 ['j']=6 ['k']=7 ['l']=8 ['m']=9)
+
+
+declare -a octave0=("16,35" "17,33" "18,36" "19,45" "20,60" "21,83" "23,13" "24,50" "25,96" "27,50" "29,14" "30,87")
+
 while true; do
 read -rsn1 input
-if [ "$input" = "q" ];then
-	beep -l 150 -f 220 &
-fi
-if [ "$input" = "s" ];then
-	beep -l 150 -f 280 &
-fi
-if [ "$input" = "d" ];then
-	beep -l 150 -f 300 &
-fi
-if [ "$input" = "f" ];then
-	beep -l 150 -f 315 &
-fi
-if [ "$input" = "g" ];then
-	beep -l 150 -f 330 &
-fi
-if [ "$input" = "h" ];then
-	beep -l 150 -f 350 &
-fi
-if [ "$input" = "j" ];then
-	beep -l 150 -f 375 &
-fi
-if [ "$input" = "k" ];then
-	beep -l 150 -f 405 &
-fi
-if [ "$input" = "l" ];then
-	beep -l 150 -f 450 &
-fi
-if [ "$input" = "m" ];then
-	beep -l 150 -f 550 &
+if [ "$input" != "" ];then
+	echo ${octave0[${idx[$input]}]}
 fi
 done
